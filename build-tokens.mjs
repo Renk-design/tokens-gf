e6bb15a924a24852211db1423f700ccebac61ba4
Renk Design <51901367+Renk-design@users.noreply.github.com>
2024-07-19 11:59:38 +0300

    Update build-tokens.mjs

--- build-tokens.mjs
+++ build-tokens.mjs
@@ -13,29 +13,6 @@ registerTransforms(StyleDictionary, {
   excludeParentKeys: true,
 });
 
-import { usesReferences, getReferences } from 'style-dictionary/utils';
-
-//...
-function({ dictionary }) {
-  return dictionary.allTokens.map(token => {
-    let value = JSON.stringify(token.value);
-    // the `dictionary` object now has `usesReferences()` and
-    // `getReferences()` methods. `usesReferences()` will return true if
-    // the value has a reference in it. `getReferences()` will return
-    // an array of references to the whole tokens so that you can access their
-    // names or any other attributes.
-    if (usesReferences(token.original.value)) {
-      const refs = getReferences(token.original.value, dictionary);
-      refs.forEach(ref => {
-        value = value.replace(ref.value, function() {
-          return `${ref.name}`;
-        });
-      });
-    }
-    return `export const ${token.name} = ${value};`
-  }).join(`\n`)
-}
-
 const sd = new StyleDictionary({
   source: ['tokens.json'],
   preprocessors: ['tokens-studio'],
